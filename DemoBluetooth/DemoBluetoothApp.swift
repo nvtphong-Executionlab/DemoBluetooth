05f073bd16d98d104a5ed67000005edb9785e746
nvtphong-Executionlab <phong.nguyen@executionlab.asia>
2023-06-15 09:38:26 +0700

    Initial Commit

---
+++ DemoBluetooth/DemoBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoBluetoothApp.swift
+//  DemoBluetooth
+//
+//  Created by exe-macm1 on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
